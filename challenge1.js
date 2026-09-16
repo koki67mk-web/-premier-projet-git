2d2bc5b00b1f6405e6d1b18b03eda09639b93388
koki67mk-web <koki67mk@gmail.com>
2026-09-16 17:23:14 +0100

    challenge1

---
+++ challenge1.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ');
+const age = prompt('wahat is your age? ');
+console.log(`hello ${name} , your are ${age} years old ,`);
