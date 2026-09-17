270d98ce9766c4cdf60bee92d768a4c199ac65bc
koki67mk-web <koki67mk@gmail.com>
2026-09-17 16:08:19 +0100

    condition2

--- condition/condition2.js
+++ condition/condition2.js
@@ -1,3 +1,16 @@
 //Écrivez un programme JavaScript qui vérifie si un caractère saisi par l'utilisateur est une voyelle ou non
 //  en utilisant l'instruction switch.
 const prompt = require('prompt-sync')();
+let voyelle = prompt("entrée un letre ");
+switch (voyelle){
+    case "a" :
+    case "e" :
+    case "u" :
+    case "o" :
+    case "i" :
+        console.log("une voyelle");
+        break;
+        default:
+            console.log( "la letre pas une voyelle ")
+            // la letre pas une voyelles
+}
