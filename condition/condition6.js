36bb7b5275a1259581409259658486e3ae93d4b2
koki67mk-web <koki67mk@gmail.com>
2026-09-17 16:53:08 +0100

    condition6

---
+++ condition/condition6.js
@@ -0,0 +1,12 @@
+//Écrivez un programme pour vérifier si le nombre donné est positif, négatif ou nul.
+//  Si le nombre est inférieur à zéro, alors le nombre est négatif et si le nombre est supérieur à zéro,
+//  alors le nombre est positif. Si les deux conditions sont fausses, le nombre est égal à zéro.
+const prompt = require('prompt-sync')();
+let num = Number(prompt("entrée un nombr "));
+if (num < 0){
+    console.log(" négatif ");
+}else if (num > 0){
+    console.log(" positif ");
+}else{
+    console.log(" egal zero ");
+}
