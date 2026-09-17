2a1c0f8e9a41ff7b5cd3987f0b06bd9a1cb7ff3a
koki67mk-web <koki67mk@gmail.com>
2026-09-17 15:31:58 +0100

    condition3

---
+++ condition/condition2.js
@@ -0,0 +1,3 @@
+//Écrivez un programme JavaScript qui vérifie si un caractère saisi par l'utilisateur est une voyelle ou non
+//  en utilisant l'instruction switch.
+const prompt = require('prompt-sync')();
---
+++ condition/condition3.js
@@ -0,0 +1,11 @@
+//Écrivez un programme JavaScript pour calculer la somme de deux valeurs entières données.
+//  Si les deux valeurs sont identiques, le programme doit renvoyer le triple de leur somme.
+const prompt = require('prompt-sync')();
+let var1 = Number(prompt("entrée un valeur "));
+let var2 = Number(prompt("entrée un valeur "));
+if (var1 === var2){
+    let var3 = (var1+var2)*3;
+    console.log("resulta" + " " +var3)
+}else{
+    console.log("la valeur est deférant");
+}
