33c11b31e1e3a1a1542a43ed10b9575c2eec573e
koki67mk-web <koki67mk@gmail.com>
2026-09-18 12:01:13 +0100

    boucles4

--- boucles/boucles4.js
+++ boucles/boucles4.js
@@ -1,3 +1,11 @@
 //Écrivez un programme JavaScript qui demande un nombre entier n et affiche
 //  les n premiers nombres impairs. Par exemple, pour n = 5, affichez : 1, 3, 5, 7, 9.
 const prompt = require('prompt-sync')();
+let n = Number(prompt(" entrer un nombre "));
+let num = 2 ;
+for(let i = 1 ; i <= n ; i++){
+    let res = 0;
+    res = i * num  -1 ;
+    console.log( res )
+}
+
---
+++ boucles/boucles6.js
@@ -0,0 +1,4 @@
+//Écrivez un programme JavaScript qui demande un nombre entier n et affiche 
+// les n premiers nombres pairs. Par exemple, pour n = 4, affichez : 2, 4, 6, 8.
+const prompt = require('prompt-sync')();
+let num = Number(prompt("entrer un nombre"));
