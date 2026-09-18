793d6d44845ebca12f44d7ec5c6be95290458158
koki67mk-web <koki67mk@gmail.com>
2026-09-18 12:08:02 +0100

    boucles6

--- boucles/boucles6.js
+++ boucles/boucles6.js
@@ -1,4 +1,10 @@
 //Écrivez un programme JavaScript qui demande un nombre entier n et affiche 
 // les n premiers nombres pairs. Par exemple, pour n = 4, affichez : 2, 4, 6, 8.
 const prompt = require('prompt-sync')();
-let num = Number(prompt("entrer un nombre"));
+let n = Number(prompt(" entrer un nombre "));
+let nom = 2 ;
+for (let i = 1 ;i<= n ; i++){
+    let s = 0;
+    s = i * nom ;
+    console.log(s)
+}
