6ad4234e981cf5ad65995dfb9da3bd7de13be0ca
koki67mk-web <koki67mk@gmail.com>
2026-09-21 14:42:28 +0000

    chaines8

--- ChainesCaractères/chaines8.js
+++ ChainesCaractères/chaines8.js
@@ -1,2 +1,5 @@
 //Écrivez un programme JavaScript qui lit une chaîne de caractères et convertit
 //  tous les caractères en minuscules. Affichez la chaîne modifiée.
+const prompt = require("prompt-sync")();
+let text = prompt(" entré un texts ").toLowerCase()
+console.log(text);
