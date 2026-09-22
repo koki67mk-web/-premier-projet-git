4def667b4a46fee8564cbb97ef9ecf738d880a03
koki67mk-web <koki67mk@gmail.com>
2026-09-22 12:34:08 +0000

    fonctions2

---
+++ fonctions/fonctions2.js
@@ -0,0 +1,6 @@
+//Écrivez une fonction JavaScript qui prend deux nombres en paramètres et retourne 
+// leur produit. Utilisez cette fonction pour afficher le produit de deux nombres.
+function multiblucation ( nombre1 , nombre2){
+    return nombre1 * nombre2
+};
+   console.log(multiblucation(10 , 15));
