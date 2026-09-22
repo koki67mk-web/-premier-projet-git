c7ab4aae156fc52c2b79a5a0ca7b7ed742b873c8
koki67mk-web <koki67mk@gmail.com>
2026-09-22 12:24:05 +0000

    fonctions1

---
+++ fonctions/fonctions1.js
@@ -0,0 +1,7 @@
+//Écrivez une fonction JavaScript qui prend deux nombres en paramètres et retourne leur somme.
+//  Utilisez cette fonction pour afficher la somme de deux nombres.
+function somme( a , b ){
+    return a+b 
+};
+console.log(somme( 13 , 10));
+
